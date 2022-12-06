f1414bb921d5828e75217ea905caae746402c057
Du, Jun <jun.du@intel.com>
2022-12-06 09:01:41 +0000

    update easyRec

---
+++ launch_benchmark.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+set -xe
+
+function main {
+    # set common info
+    source common.sh
+    init_params $@
+    fetch_device_info
+    set_environment
+
+    # requirements
+    ln -sf /home2/tensorflow-broad-product/oob_tf_models/EasyRec/dataset dataset
+    pip uninstall easy-rec -y
+    python setup.py install
+    if [ -d "experiments" ];then
+        rm -rf experiments
+    fi
+
+    # if multiple use 'xxx,xxx,xxx'
+    model_name_list=($(echo "${model_name}" |sed 's/,/ /g'))
+    batch_size_list=($(echo "${batch_size}" |sed 's/,/ /g'))
+
+    # generate benchmark
+    for model_name in ${model_name_list[@]}
+    do
+        #
+        for batch_size in ${batch_size_list[@]}
+        do
+            # clean workspace
+            logs_path_clean
+            generate_core
+            # launch
+            echo -e "\n\n\n\n Running..."
+            #cat ${excute_cmd_file} |column -t > ${excute_cmd_file}.tmp
+            #mv ${excute_cmd_file}.tmp ${excute_cmd_file}
+            source ${excute_cmd_file}
+            echo -e "Finished.\n\n\n\n"
+            # collect launch result
+            collect_perf_logs
+        done
+    done
+}
+
+# run
+function generate_core {
+    # generate multiple instance script
+    for(( i=0; i<instance; i++ ))
+    do
+        real_cores_per_instance=$(echo ${device_array[i]} |awk -F, '{print NF}')
+        log_file="${log_dir}/rcpi${real_cores_per_instance}-ins${i}.log"
+
+        # instances
+        if [ "${device}" == "cpu" ];then
+            OOB_EXEC_HEADER=" numactl -m $(echo ${device_array[i]} |awk -F ';' '{print $2}') "
+            OOB_EXEC_HEADER+=" -C $(echo ${device_array[i]} |awk -F ';' '{print $1}') "
+        elif [ "${device}" == "cuda" ];then
+            OOB_EXEC_HEADER=" CUDA_VISIBLE_DEVICES=${device_array[i]} "
+        fi
+        printf " ${OOB_EXEC_HEADER} \
+	    python easy_rec/python/train_eval.py --pipeline_config_path essm.config \
+                ${addtion_options} \
+        > ${log_file} 2>&1 &  \n" |tee -a ${excute_cmd_file}
+    done
+    echo -e "\n wait" >> ${excute_cmd_file}
+}
+
+# download common files
+timeout 10s wget -q -O common.sh https://raw.githubusercontent.com/mengfei25/oob-common/gpuoob/common.sh || cp /tmp/common.sh .
+
+# Start
+main "$@"
+
